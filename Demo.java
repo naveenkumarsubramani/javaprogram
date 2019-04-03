46fab2fb3a8be1782ab3364df183a2764da0cfc1
naveenkumarsubramani <srnaveenkumar96@gmail.com>
2019-04-03 13:16:04 -0700

    this is my first program

---
+++ Demo.java
@@ -0,0 +1,13 @@
+class Demo
+{
+int number;
+String name;
+Demo(int i,String s)
+{
+System.out.println("integer value:"+i+" string name:" +s);
+}
+public static void main(String args[])
+{
+Demo obj=new Demo(55,"ram");
+}
+}
